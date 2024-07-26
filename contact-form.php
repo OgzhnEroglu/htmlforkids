b560f0764c99e37d6b3d9e2b0e9b724c91614260
OgzhnEroglu <ooguzhanerogluu@gmail.com>
2024-07-26 04:20:28 +0300

    Added team members. Contact-form.php Security hardening.

--- contact-form.php
+++ contact-form.php
@@ -1,42 +1,43 @@
 <?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$emailHelp = $_POST['email'];
-$comments = $_POST['comments'];
+function sanitize_input($data)
+{
+	return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
+}
 
-if (isset($name) && isset($phone) && isset($emailHelp)) {
+$name = isset($_POST['name']) ? sanitize_input($_POST['name']) : '';
+$phone = isset($_POST['phone']) ? sanitize_input($_POST['phone']) : '';
+$emailHelp = isset($_POST['email']) ? sanitize_input($_POST['email']) : '';
+$comments = isset($_POST['comments']) ? sanitize_input($_POST['comments']) : '';
+
+if (!empty($name) && !empty($phone) && !empty($emailHelp) && filter_var($emailHelp, FILTER_VALIDATE_EMAIL)) {
 	global $to_email, $vpb_message_body, $headers;
-	$to_email = "";
-	// $email_subject="Inquiry From Contact Page";
+	$to_email = "ooguzhanerogluu@gmail.com";
+
 	$vpb_message_body = nl2br("Dear Admin,\n
-	The user whose detail is shown below has sent this message from " . $_SERVER['HTTP_HOST'] . " dated " . date('d-m-Y') . ".\n
-	
-	name: " . $name . "\n
-	Email Address: " . $emailHelp . "\n
-    Phone: " . $subject . "\n
+    The user whose detail is shown below has sent this message from " . $_SERVER['HTTP_HOST'] . " dated " . date('d-m-Y') . ".\n
 
-	Message: " . $comments . "\n
-	User Ip:" . getHostByName(getHostName()) . "\n
-	Thank You!\n\n");
+    Name: " . $name . "\n
+    Email Address: " . $emailHelp . "\n
+    Phone: " . $phone . "\n
+    Message: " . $comments . "\n
+    User IP: " . $_SERVER['REMOTE_ADDR'] . "\n
+    Thank You!\n\n");
 
-	//Set up the email headers
-	$headers    = "From: $name <$emailHelp>\r\n";
-	$headers   .= "Content-type: text/html; charset=iso-8859-1\r\n";
-	$headers   .= "Message-ID: <" . time() . rand(1, 1000) . "@" . $_SERVER['SERVER_NAME'] . ">" . "\r\n";
+	// Set up the email headers
+	$headers  = "From: $name <$emailHelp>\r\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+	$headers .= "Message-ID: <" . time() . rand(1, 1000) . "@" . $_SERVER['SERVER_NAME'] . ">\r\n";
 
-	if (@mail($to_email, $vpb_message_body, $headers)) {
+	if (@mail($to_email, "Inquiry From Contact Page", $vpb_message_body, $headers)) {
 		$status = 'Success';
-		//Displays the success message when email message is sent
-		$output = "Congrats " . $name . ", your email message has been sent successfully! We will get back to you as soon as possible. Thanks.";
+		$output = "Congrats " . htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . ", your email message has been sent successfully! We will get back to you as soon as possible. Thanks.";
 	} else {
 		$status = 'error';
-		//Displays an error message when email sending fails
-		$output = "Sorry, your email could not be sent at the moment. Please try again or contact this website admin to report this error message if the problem persist. Thanks.";
+		$output = "Sorry, your email could not be sent at the moment. Please try again or contact this website admin to report this error message if the problem persists. Thanks.";
 	}
 } else {
-
-	echo $name;
 	$status = 'error';
-	$output = "please fill require fields";
+	$output = "Please fill in all required fields with valid information.";
 }
+
 echo json_encode(array('status' => $status, 'msg' => $output));
